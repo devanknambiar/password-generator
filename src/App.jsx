d604e796a34874f0f7b50c77c0672c89220d538b
devanknambiar <162826978+devanknambiar@users.noreply.github.com>
2025-07-26 12:24:51 +0530

    Add Regenerate Button

--- src/App.jsx
+++ src/App.jsx
@@ -33,25 +33,39 @@ function App() {
 
   const pswdCopy = () => {
     navigator.clipboard.writeText(pswdRef.current.value)
+    pswdRef.current.select()
+  }
+
+  const regeneratePswd = () => {
+    pswdGenerator()
   }
 
   return (
     <>
       <div className="flex justify-center items-center min-h-screen">
-        <div className="w-full max-w-md px-8 py-4 rounded-lg justify-center text-3xl bg-gray-700 ">
+        <div className="w-full max-w-lg px-8 py-4 rounded-lg text-3xl bg-gray-700">
           <div className="flex gap-x-2 items-center">
             <h1 className='flex-none text-white'>Password Generator</h1>
             <h1 className="ml-auto">🔐</h1>
           </div>
           <div className="flex overflow-hidden mt-4 rounded-xl">
+            <div className="relative w-full">
             <input 
             type="text"
             placeholder='password'
-            className='outline-none w-full bg-white px-2 py-3 cursor-not-allowed'
+            className='w-full bg-white pl-2 pr-14 py-3 cursor-not-allowed'
             ref={pswdRef}
             value={pswd}
             readOnly
             />
+            <div className="flex w-14 items-center justify-center inset-y-0  right-0 absolute cursor-pointer">
+              <img 
+              src="./src/assets/loader.svg" alt="regenerate" 
+              className='h-10 hover:animate-oppspin'
+              onClick={regeneratePswd}
+              />
+            </div>
+            </div>
             <button
             className='bg-blue-700 hover:bg-blue-500 active:text-black transition-colors text-white outline-none shrink-0 px-2 py-0.5'
             onClick={pswdCopy}
